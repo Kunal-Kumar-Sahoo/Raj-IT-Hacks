aa37427c5d397fe52964fd3f8a7d5b17978e26a4
Kunal Kumar Sahoo <kunal.sahoo2003@gmail.com>
2023-03-21 05:51:26 +0530

    readme commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Laxmi Chit Funds
+Developed by:
+- Anmol Singh
+- Dhruvi Gorakhia
+- Kunal Kumar Sahoo
+- Margi Yadav
+- Piyush Tilokchandani
+
+### Problem Statement:
+
+### Our solution and relevance: 
+1. Insurance: 
+   will provide insurance to small scale startups which will encourage them to step into business world.
+   Insurance ensures them that money loss is no more a barrier between them and there startup.  
+2. Funding:
+   lack of proper funding is an issue faced by every small scale startup owners.
+   To escape tidious bank processes they opt for money lenders which inreturn demands high interest rate.
+3. Risk management: 
+   Our product also aims to minimise the risk. Designed ML model is trained in such a way that it can predict the risk in your startup and will warn you before hand.
+
+### Technologies used in our solution:
+- MySQL
+- Flask
+- ReactJS
+
+### How to run the software:
+1. Clone the project repository.
+   ```bash
+   git clone git@<name of the repository>.git
+   ```
+2. Install the frontend dependencies:
+```bash
+    cd ./src/frontend/
+    npm install
+    npm start
+```
+3. Install the backend dependencies
+```python
+    pip install -r requirements.txt
+    python3 ./src/backend/app.py
+```
--- src/backend/app.py
+++ src/backend/app.py
@@ -82,5 +82,64 @@ def submit_problem():
     problem_location = request_data['location']
     queries.write_problem(problem_description, problem_location)
 
+
+@app.route('/premium_to_insurance', methods=['POST', 'GET'])
+def pay_premium():
+    request_data = request.get_json()
+    email_id= request_data['email']
+    funding, valuation, revenue = queries.get_monetary_details(email_id)
+    
+    roi = (valuation - revenue) / valuation
+    
+    premium_percent = 100
+
+    if roi == -1:
+        premium_percent = 20
+    elif roi > -1 and roi <= -.90:
+        premium_percent = 19
+    elif roi > -0.90 and roi <= -0.80:
+        premium_percent = 18
+    elif roi > -0.80 and roi <= -0.70:
+        premium_percent = 17
+    elif roi > -0.70 and roi <= -0.60:
+        premium_percent = 16
+    elif roi > -0.6 and roi <= -0.5:
+        premium_percent = 15
+    elif roi> -0.50 and roi<=-0.40:
+        premium_percent = 14
+    elif roi> -0.40 and roi<=-0.0:
+        premium_percent = 13
+    elif roi > -0.30 and roi<=-020:
+        premium_percent = 12
+    elif roi> -0.20 and roi<=-.10:
+        premium_percent = 11
+    elif roi > -0.10 and roi<=0.0:
+        premium_percent = 10
+    elif roi >0 and roi<=0.10:
+       premium_percent = 9
+    elif roi > 0.10 and roi<=0.20
+        premium_percent = 8
+    elif roi >0.20 and roi<=0.20:
+        premium_percent = 7
+    elif roi >0.30 and roi<=0.40:
+        premium_percent = 6
+    elif roi >0.40 and roi<=0.50:
+        premium_percent = 5   
+    elif roi >0.50 and roi<=0.60:
+        premium_percent = 4
+    elif roi >0.60 and roi<=0.70:
+        premium_percent = 3
+    elif roi >0.70 and roi<=0.80:
+        premium_percent = 2  
+    elif roi >0.180 and roi<=0.9:
+        premium_percent = 1  
+    else:
+        premium_percent = 0
+
+    premium = (premium_percent * funding)/100
+
+    return premium
+
+
 if __name__ == '__main__':
     app.run()
